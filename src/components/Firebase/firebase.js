abd42012537b14592819a7aab88871a4dfa40ece
sheilahd <sheila.hdez@gmail.com>
2021-01-06 23:21:15 -0500

    firebase console

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -3,8 +3,6 @@ import "firebase/auth";
 import "firebase/database";
 import "firebase/storage";
 
-console.log(process.env);
-
 const config = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
